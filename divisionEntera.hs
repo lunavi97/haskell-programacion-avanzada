44090df8febdde8d0a62cfa8e4e44df255261ccb
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-06-04 03:32:41 -0300

    Devolver indefinido e indeterminado

--- divisionEntera.hs
+++ divisionEntera.hs
@@ -1,1 +1,5 @@
-division(a, b) = if a < b then 0 else 1 + division(a - b, b)
+division(a, b) =
+    if a == 0 && b == 0 then error "Indeterminado"
+    else if b == 0 then error "Indefinido"
+    else if a < b then 0
+    else 1 + division(a - b, b)
